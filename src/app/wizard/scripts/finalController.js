ff4ab49e867c513ad51ccb1bb1e416dfb365d8e5
Pral2a <guillem@pral2a.com>
2016-11-22 16:01:02 +0100

    Update 2016-11-22T15:00:53.972Z

--- src/app/wizard/scripts/finalController.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('app').controller('finalController', function($scope, $rootScope, scopePayload, AnimationService){
-    $scope.$parent.payload = scopePayload;
-    AnimationService.animate(scopePayload.index);
-    
-    // Maybe we use it for the final slide
- 
-});
